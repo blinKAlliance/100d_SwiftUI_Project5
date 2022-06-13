779bd2b7d2178493fe581ebe9b8943f32ab73c40
Jordan Kramer <45769925+blinKAlliance@users.noreply.github.com>
2022-06-13 13:16:38 -0500

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jordan Kramer on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
